445d7878ebda0022de1be1db5bbbfef6610c16eb
Wafa Saefulhaq <saefulhaqwafa@gmail.com>
2022-08-16 16:20:49 +0700

    adding new variations into getMaxSubSum

--- array/getMaxSubSum.js
+++ array/getMaxSubSum.js
@@ -5,7 +5,7 @@
  * @source: https://javascript.info/array
  */
 
-function getMaxSubSum(arr) {
+/* function getMaxSubSum(arr) {
   let maxSum = 0;
   let partialSum = 0;
 
@@ -23,8 +23,63 @@ function getMaxSubSum(arr) {
   }
 
   return maxSum;
+} */
+
+// empty subarrays admitted
+/* function getMaxSubSum(arr) {
+  let bestSum = 0;
+  let currentSum = 0;
+
+  for (const number of arr) {
+    currentSum = Math.max(0, currentSum + number);
+    console.log(`currentSum is ${currentSum}`);
+    bestSum = Math.max(bestSum, currentSum);
+    console.log(`bestSum is ${bestSum}`);
+  }
+
+  return bestSum;
+} */
+
+// no empty subarrays admitted by returning empty array
+/* function getMaxSubSum(arr) {
+  if (arr.length === 0) {
+    return arr;
+  }
+
+  let bestSum = 0;
+  let currentSum = 0;
+
+  for (const number of arr) {
+    currentSum = Math.max(0, currentSum + number);
+    console.log(`currentSum is ${currentSum}`);
+    bestSum = Math.max(bestSum, currentSum);
+    console.log(`bestSum is ${bestSum}`);
+  }
+
+  return bestSum;
+} */
+
+// conditionally admitting empty subarray
+function getMaxSubSum(arr, admitEmptySubarray = true) {
+  if (!admitEmptySubarray && arr.length === 0) {
+    return null;
+  }
+
+  let bestSum = 0;
+  let currentSum = 0;
+
+  for (const number of arr) {
+    currentSum = Math.max(0, currentSum + number);
+    console.log(`currentSum is ${currentSum}`);
+    bestSum = Math.max(bestSum, currentSum);
+    console.log(`bestSum is ${bestSum}`);
+  }
+
+  return bestSum;
 }
 
 console.log(getMaxSubSum([-1, 2, 3, -9])); // 5
 console.log(getMaxSubSum([-1, 2, 3, -9, 11])); // 11
 console.log(getMaxSubSum([-1, -2, -3])); // 0
+console.log(getMaxSubSum([])); // 0
+console.log(getMaxSubSum([], false)); // null
